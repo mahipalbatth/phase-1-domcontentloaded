6e09ce0d4a3888de19ef0a082fb5f258cbc19690
Mahipal Signh <msbatth14@gmail.com>
2022-09-22 16:39:00 -0500

    commit tree cf7907552f4e7c0e592578fafe54c39f309094e2

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    document.querySelector('p').innerHTML = "This is really cool!";
+  });
+  
+  console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+  );
+// const element = document.createElement('p');
+// element.id = "text";
+// element.innerHTML = "JavaScript is so cool. It lets me add text to my page programmatically.This is really cool!";
+
